01240808760baa75d8b5f1f096bdef8a7f5425b5
kgpksh <tkdgns543@gmail.com>
2023-11-15 04:13:34 +0900

    feat : 스프링 시큐리티 테스트용 초기 설정, 회원 및 h2 허용

---
+++ src/main/java/com/example/jwtStudy/security/SecurityConfig.java
@@ -0,0 +1,26 @@
+package com.example.jwtStudy.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        return httpSecurity
+                .httpBasic(httpBasicConfigurer -> httpBasicConfigurer.disable())
+                .csrf(csrfConfigurer -> csrfConfigurer.disable())
+                .authorizeHttpRequests(requests -> {
+                    requests.requestMatchers(
+                            new AntPathRequestMatcher("/member/**"),
+                            new AntPathRequestMatcher("/h2-console/**")).permitAll();
+                    requests.anyRequest().authenticated();
+                })
+                .build();
+    }
+}
