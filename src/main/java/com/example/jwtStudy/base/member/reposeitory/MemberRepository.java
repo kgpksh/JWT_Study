f72990d5cc648624d9b167f1fe6b71b3b4adc91c
kgpksh <tkdgns543@gmail.com>
2023-11-15 05:27:55 +0900

    feat : 레포지터리에서 회원 이름으로 찾기 구현

--- src/main/java/com/example/jwtStudy/base/member/reposeitory/MemberRepository.java
+++ src/main/java/com/example/jwtStudy/base/member/reposeitory/MemberRepository.java
@@ -3,5 +3,8 @@ package com.example.jwtStudy.base.member.reposeitory;
 import com.example.jwtStudy.base.member.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface MemberRepository extends JpaRepository<Member, Long> {
+    Optional<Member> findMemberByUsername(String username);
 }
