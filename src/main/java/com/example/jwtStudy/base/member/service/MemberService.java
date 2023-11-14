464e174c55f1b9e0e4517e5823e5bd875eaf6f43
kgpksh <tkdgns543@gmail.com>
2023-11-15 06:42:28 +0900

    feat : 회원 서비스에서 회원 가입 및 username으로 조회 구현

--- src/main/java/com/example/jwtStudy/base/member/service/MemberService.java
+++ src/main/java/com/example/jwtStudy/base/member/service/MemberService.java
@@ -1,4 +1,44 @@
 package com.example.jwtStudy.base.member.service;
 
+import com.example.jwtStudy.base.member.entity.Member;
+import com.example.jwtStudy.base.member.reposeitory.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.sql.SQLDataException;
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
 public class MemberService {
+    private final MemberRepository memberRepository;
+    private final  PasswordEncoder passwordEncoder;
+
+    @Transactional
+    public Member join(String username, String password) {
+        Member member = new Member();
+        member.setUsername(username);
+        member.setPassword(passwordEncoder.encode(password));
+        memberRepository.save(member);
+
+        return member;
+    }
+
+    public Member checkMember(String username, String password) throws Exception {
+        Optional<Member> queriedMember = memberRepository.findMemberByUsername(username);
+        if (queriedMember.isEmpty()) {
+            throw new SQLDataException("존재 하지 않는 회원입니다.");
+        }
+
+        Member member = queriedMember.get();
+
+        if (!passwordEncoder.matches(password, member.getPassword())) {
+            throw new SQLDataException("비밀번호가 틀렸습니다.");
+        }
+
+        return member;
+    }
 }
---
+++ src/test/java/com/example/jwtStudy/member/MemberTests.java
@@ -0,0 +1,28 @@
+package com.example.jwtStudy.member;
+
+import com.example.jwtStudy.base.member.entity.Member;
+import com.example.jwtStudy.base.member.service.MemberService;
+import jakarta.transaction.Transactional;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import static org.assertj.core.api.Assertions.*;
+
+@SpringBootTest
+@Transactional
+public class MemberTests {
+    @Autowired
+    MemberService memberService;
+    @Test
+    @DisplayName("회원 가입 테스트")
+    void joinTest() {
+        memberService.join("관리자", "1234");
+        try {
+            Member member = memberService.checkMember("관리자", "1234");
+            assertThat(member.getUsername()).isEqualTo("관리자");
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
